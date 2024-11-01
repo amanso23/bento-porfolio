d14cda55ac396a3c73339467638a8b37d8f8f007
Adrian <adrianmanso80@gmail.com>
2024-11-01 18:34:32 +0000

    Add About component, update Bento layout, and enhance BentoTimeCard styling for improved user experience

--- src/components/BentoTimeCard.tsx
+++ src/components/BentoTimeCard.tsx
@@ -15,7 +15,8 @@ const BentoTimeCard = () => {
 
     const getLocation = () => {
         const countryValue = country[countryKey]
-        return `${city}, ${region}, ${countryValue}`.toUpperCase()
+        return `${region}, ${countryValue}`.toUpperCase()
+        // return `${city}, ${region}, ${countryValue}`.toUpperCase()
     }
 
     const [currentTime, setCurrentTime] = useState<Date | null>(null)
@@ -34,8 +35,6 @@ const BentoTimeCard = () => {
         if(hours < 18) return "images/morning.webp"
         if(hours < 20) return "images/late-night.webp"
         return "images/night.webp"
-        
-        
     }
 
     const regionConfig = `${language}-${region}`
@@ -54,17 +53,17 @@ const BentoTimeCard = () => {
     }, [])
 
     return (
-        <BentoCard className="col-span-2 row-span-1 overflow-hidden transition-opacity duration-200 hover:opacity-80 group" redirect>
+        <BentoCard className="col-span-1 row-span-1 overflow-hidden transition-opacity duration-200 hover:opacity-80 group" redirect>
             <a href={linkToMaps} target="_blank" rel="noopener noreferrer">
                 <picture className="absolute top-0 left-0 w-full h-full object-cover">
-                    <img src={getPicture()} alt="picture" className="w-full object-cover z-0" />
+                    <img src={getPicture()} alt="picture" className="w-full h-full object-cover z-0" />
                 </picture>
-                <div className="flex flex-col gap-y-2  items-start font-bold z-50 relative " >
-                    <h2 className="text-5xl">{getGreeting()}</h2>
-                    <h1 className="text-8xl">
+                <div className="flex flex-col gap-y-4 h-full items-start font-bold z-50 relative" >
+                    <h2 className="text-3xl">{getGreeting()}</h2>
+                    <h1 className="text-5xl">
                         {currentTime ? currentTime.toLocaleTimeString(regionConfig, options) : ""}
                     </h1>
-                    <p className="text-lg uppercase backdrop-blur-sm bg-white/10 px-4 py-2 rounded-full">{getLocation()}</p>
+                    <p className="uppercase backdrop-blur-sm bg-white/40 px-4 py-2 rounded-full">{getLocation()}</p>
                 </div>
             </a>     
         </BentoCard>
