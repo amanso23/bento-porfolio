d54b8a395c5357fcc1209bbee6a87dbc9afc78c3
Adrian <adrianmanso80@gmail.com>
2024-11-05 14:14:59 +0000

    Refactor Bento layout for improved responsiveness, update styles in BentoStackCard and BentoTimeCard, and remove unused StackCard component

--- src/components/BentoTimeCard.tsx
+++ src/components/BentoTimeCard.tsx
@@ -64,7 +64,7 @@ const BentoTimeCard = () => {
                     {currentTime ? <div className="flex items-center gap-x-1"><h1 className="text-6xl">{currentTime.toLocaleTimeString(regionConfig, options)} </h1><p className="text-3xl mt-5">{seconds}</p></div> : ""}
                     <a 
                         href={linkToMaps} target="_blank" rel="noopener noreferrer"
-                        className="group uppercase backdrop-blur-sm bg-white/40 px-3 py-2 rounded-full transition-colors duration-200 hover:bg-white/60"  
+                        className="group uppercase backdrop-blur-sm bg-white/40 px-3 py-2 rounded-full transition-colors duration-200 hover:bg-white/60 text-sm"  
                     >
                         {getLocation()}
                         <Redirect className="stroke-white inline-block size-5 mb-[2px] ml-1 transition-transform duration-300 group-hover:transform group-hover:-translate-y-1 group-hover:translate-x-1 " />
